178ca6fef06f404be1b8b5c62b40096df4f19700
paulinare <43135750+paulinare@users.noreply.github.com>
2018-09-12 18:05:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const navButton = document.getElementById("toggle-menu-btn");
+const buttonIcon = document.getElementById("button-icon");
+
+const toggleClassNavButton = () => {
+  if (buttonIcon.classList.contains("fa-bars")){
+    buttonIcon.classList.remove("fa-bars");
+    buttonIcon.classList.add("fa-times");  
+    document.getElementById("painting-container").style.animation = "show 0.9s forwards"; 
+    document.getElementById("hidden-nav").style.animation = "show 0.9s forwards"; 
+  }  
+  else {
+    buttonIcon.classList.remove("fa-times");
+    buttonIcon.classList.add("fa-bars"); 
+    document.getElementById("painting-container").style.animation = "hide 0.7s forwards"; 
+     document.getElementById("hidden-nav").style.animation = "hide 0.7s forwards";  
+     
+  }
+}
+ 
+navButton.onclick = function() { 
+  toggleClassNavButton(); 
+}   
+
